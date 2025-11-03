5d97f3eb5e11341a9b6a78ce37ea5d66fc8d3496
ASC18-PRIYA-GIT-HUB <priyapalraj102004@gmail.com>
2025-11-03 17:27:10 +0530

    Update Screenshots.java

--- snapdeal_appium/Project/src/main/java/com/snap/Screenshots.java
+++ snapdeal_appium/Project/src/main/java/com/snap/Screenshots.java
@@ -1,19 +1,30 @@
-package com.snap;
+package com.snapdeal.utilities;
 
-import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-public class Screenshots {
-	static String projectpath=System.getProperty("user.dir");
-	public static String Capture(WebDriver driver, String TestName) throws IOException
-	{
-		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String dest=projectpath+"\\src\\test\\resources\\Screenshots\\sc1.png";
-		File destfile=new File(dest);
-		FileUtils.copyFile(src, destfile);
-		return dest;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenshotUtil {
+    static String projectPath = System.getProperty("user.dir");
+
+    public static String capture(WebDriver driver, String testName) throws IOException {
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String dest = projectPath + "\\src\\test\\resources\\Screenshots\\" + testName + ".png";
+        File destFile = new File(dest);
+        FileUtils.copyFile(src, destFile);
+        System.out.println("📸 Screenshot saved at: " + dest);
+        return dest;
+    }
+
+	public static void takeScreenshot(AndroidDriver<MobileElement> driver, String string) {
+		// TODO Auto-generated method stub
+		
 	}
 }
