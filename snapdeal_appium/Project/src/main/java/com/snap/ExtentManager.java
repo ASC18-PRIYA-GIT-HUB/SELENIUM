f28a3f9771527a3d72eabf39d53e71d1574b9781
ASC18-PRIYA-GIT-HUB <priyapalraj102004@gmail.com>
2025-11-03 17:26:44 +0530

    Update ExtentManager.java

--- snapdeal_appium/Project/src/main/java/com/snap/ExtentManager.java
+++ snapdeal_appium/Project/src/main/java/com/snap/ExtentManager.java
@@ -1,18 +1,21 @@
-package com.snap;
+package com.snapdeal.utilities;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
- 
+
 public class ExtentManager {
-	private static ExtentReports extent;
-	static String projectpath = System.getProperty("user.dir"); // project path
-	public static ExtentReports getinstance() {
-		if (extent == null) {
-			String reportpath = projectpath + "\\src\\test\\resources\\Reports\\snapdealreport.html";
-			ExtentSparkReporter spark = new ExtentSparkReporter(reportpath);
-			extent = new ExtentReports();
-			extent.attachReporter(spark);
-		}
-		return extent;
-	}
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String projectPath = System.getProperty("user.dir");
+            String reportPath = projectPath + "\\src\\test\\resources\\Reports\\Snapdeal_Report.html";
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            spark.config().setDocumentTitle("Snapdeal Automation Report");
+            spark.config().setReportName("Snapdeal Android Automation");
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+        }
+        return extent;
+    }
 }
